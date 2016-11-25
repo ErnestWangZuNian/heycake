ea01f76f417c88c5eaac62fbca2ad496454a0e60
mafeiyan <83452600@qq.com>
2016-11-25 15:12:51 +0800

    productsList相关

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 // require.ensure 是 Webpack 的特殊语法，用来设置 code-split point
 const Test = resolve => require(['../components/Test'], resolve)
 const Index = resolve => require(['../components/Index'], resolve)
+const ProductsList = resolve => require(['../components/ProductsList'], resolve)
 const Bar = { template: '<div>bar</div>' }
 const routes = [
     { path: '/site/bar', component: Bar },
     { path: '/site/test', component: Test },
-    { path: '/site/Index', component: Index }
+    { path: '/site/index', component: Index },
+    { path: '/site/productsList', component: ProductsList }
 ]
 export default routes
