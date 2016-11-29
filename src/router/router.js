0f2dfd39ef1a996ab840fb248e6f31dfc5850113
mafeiyan <83452600@qq.com>
2016-11-29 17:55:43 +0800

    新增收货地址页面

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ const NewArrivals = resolve => require(['../components/NewArrivals'], resolve)
 const UserCenter = resolve => require(['../components/UserCenter'], resolve)
 const Cart = resolve => require(['../components/Cart'], resolve)
 const OrderSubmit = resolve => require(['../components/OrderSubmit'], resolve)
+const AddAddress = resolve => require(['../components/AddAddress'], resolve)
 const Loading = resolve => require(['../components/Loading'], resolve)
 const Login = resolve => require(['../components/Login'], resolve)
 const routes = [
@@ -14,6 +15,7 @@ const routes = [
     { path: '/site/user-center', component: UserCenter },
     { path: '/site/cart', component: Cart },
     { path: '/site/order-submit', component: OrderSubmit },
+    { path: '/site/add-address', component: AddAddress },
     { path: '/site/loading', component: Loading },
     { path: '/site/login', component: Login }
 ]
