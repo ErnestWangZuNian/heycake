857ebb71147968d65abfb6711c095bb93c8e7de2
mafeiyan <83452600@qq.com>
2016-12-02 17:34:21 +0800

    页面及部分公共样式

--- src/components/MyOrder.vue
+++ src/components/MyOrder.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <loading v-if="loading"></loading>
+    <loading v-if="loading && loadShow"></loading>
     <div class="container" v-if="!loading">
       <div class="grid bgc-f5f5f5 height-90">
         <div class="grid-cell tright c-888">订单筛选：</div>
         <div class="grid-cell tleft c-333">
-          <select class="selects">
+          <select class="selects" v-model='status' @change="getListData()">
             <option value="">全部</option>
-            <option value="at00">待付款</option>
+            <option value="at00">待支付</option>
             <option value="wfs0">待发货</option>
             <option value="wfd1">待收货</option>
             <option value="wfc0">待评价</option>
@@ -19,113 +19,143 @@
         </div>
       </div>
 
-      <div class="orders">
-        <div class="grid height-85 border-bottom">
-          <div class="grid-cell">下单时间：<span class="c-666">2015-5-5 13:22</span></div>
-          <div class="grid-cell tright u-1of4">待支付</div>
-        </div>
+      <template v-for='item in listData'>
+        <div class="orders">
+          <div class="grid height-85 border-bottom">
+            <div class="grid-cell">下单时间：<span class="c-666">{{item.create_time}}</span></div>
+            <div class="grid-cell tright u-1of4">{{item.status_code}}</div>
+          </div>
 
-        <ul>
-          <li class="grid height-150 border-bottom">
-            <div class="grid-cell u-w100">
-              <img src="../assets/img/goods1.jpg">
+          <ul>
+            <li class="grid height-150 border-bottom" v-for='child in item.items'>
+              <div class="grid-cell u-w100">
+                <img :src="child.cover">
+              </div>
+              <div class="grid-cell">
+                <div class="title">{{child.name}}</div>
+                <div class="c-666 f-20">
+                  <template v-for='spec in child.specifications'>
+                  {{spec}}
+                  </template>
+                </div>
+              </div>
+              <div class="grid-cell u-w130 tright">
+                <div class="price">￥{{child.price}}</div>
+                <div class="f-20 c-666">x{{child.amount}}</div>
+              </div>
+            </li>
+          </ul>
+          <div class="grid height-85 border-bottom">
+            <div class="grid-cell c-888">
+              支付金额：
+              <span class="c-red">￥{{item.total_sum}}</span>
+              <template v-if='item.logistics_mode == "pick-up"'>（自提）</template>
+              <template v-else>（含运费：{{item.logistics_price}}元）</template>
             </div>
-            <div class="grid-cell">
-              <div class="title">hey coffee 美味无敌千层大蛋糕</div>
-              <div class="c-666 f-20">6寸 13cm*13cm 2-4人</div>
+            <div class="grid-cell tright u-1of4">
+              <template v-if='item.status_code == "待支付" '><span class="btn-red">去支付</span></template>
             </div>
-            <div class="grid-cell u-w130 tright">
-              <div class="price">￥138.00</div>
-              <div class="f-20 c-666">x1</div>
-            </div>
-          </li>
-          <li class="grid height-150 border-bottom">
-            <div class="grid-cell u-w100">
-              <img src="../assets/img/goods1.jpg">
-            </div>
-            <div class="grid-cell">
-              <div class="title">hey coffee 美味无敌千层大蛋糕</div>
-              <div class="c-666 f-20">6寸 13cm*13cm 2-4人</div>
-            </div>
-            <div class="grid-cell u-w130 tright">
-              <div class="price">￥138.00</div>
-              <div class="f-20 c-666">x1</div>
-            </div>
-          </li>
-          <li class="grid height-150 border-bottom">
-            <div class="grid-cell u-w100">
-              <img src="../assets/img/goods1.jpg">
-            </div>
-            <div class="grid-cell">
-              <div class="title">hey coffee 美味无敌千层大蛋糕</div>
-              <div class="c-666 f-20">6寸 13cm*13cm 2-4人</div>
-            </div>
-            <div class="grid-cell u-w130 tright">
-              <div class="price">￥138.00</div>
-              <div class="f-20 c-666">x1</div>
-            </div>
-          </li>
-        </ul>
-        <div class="grid height-85 border-bottom">
-          <div class="grid-cell c-888">支付金额：<span class="c-red">￥316.00</span>（含运费）</div>
-          <div class="grid-cell tright u-1of4">
-            <span class="btn-red">去支付</span></div>
+          </div>
         </div>
-      </div>
-
-      <div class="splitter"></div>
-
-      <div class="orders">
-        <div class="grid height-85 border-bottom">
-          <div class="grid-cell">下单时间：<span class="c-666">2015-5-5 13:22</span></div>
-          <div class="grid-cell tright u-1of4">待支付</div>
-        </div>
-
-        <ul>
-          <li class="grid height-150 border-bottom">
-            <div class="grid-cell u-w100">
-              <img src="../assets/img/goods1.jpg">
-            </div>
-            <div class="grid-cell">
-              <div class="title">hey coffee 美味无敌千层大蛋糕</div>
-              <div class="c-666 f-20">6寸 13cm*13cm 2-4人</div>
-            </div>
-            <div class="grid-cell u-w130 tright">
-              <div class="price">￥138.00</div>
-              <div class="f-20 c-666">x1</div>
-            </div>
-          </li>
-        </ul>
-        <div class="grid height-85 border-bottom">
-          <div class="grid-cell c-888">支付金额：<span class="c-red">￥316.00</span>（含运费）</div>
-          <div class="grid-cell tright u-1of4">
-            <span class="btn-red">去支付</span></div>
-        </div>
-      </div>
+        <div class="splitter"></div>
+      </template>
 
     </div>
   </div>
 </template>
 <script>
-  import {Swipe, SwipeItem } from 'mint-ui'
+  import {Swipe, SwipeItem, MessageBox } from 'mint-ui'
   import  Loading from './Loading'
   import ajax from '../utils/ajax.js'
+  import utils from '../utils/public'
   export default {
     name: 'MyOrder',
     components: {
       Loading,
       Swipe,
       SwipeItem,
+      MessageBox
     },
     mounted () {
-
+      this.isLoginMethod()
     },
     data () {
       return {
-        loading: false,
+        loading: true,
+        loadShow: false,
+        isLogin:this.$store.state.user.userInfo.isLogin  || '',   //是否登录
+        userId:this.$store.state.user.userInfo.userId || '',      //当前用户ID
+        userInfo:{},
+        listData:[],
+        status:''
       }
     },
     methods: {
+//      判断是否登录
+      isLoginMethod(){
+        if(this.isLogin){
+          this.loadShow = true
+          this.getListData();
+        }else{
+          MessageBox.alert('未登录').then(action => {
+            location.href = '/#/site/login'
+          })
+        }
+      },
+      //获取列表数据
+      getListData(){
+        ajax.getDataFromApi({
+          url:'/v1/order',
+          data:{
+            status_code: this.status || '',
+            per_page: 20
+            }
+          },(response)=>{
+            this.listData = response.data.body.list;
+
+            //数据处理函数
+            this.dataDispose(this.listData);
+           //数据请求完成,改变loading值,关闭load，显示渲染后的页面
+            this.loading = false;
+          })
+      },
+      //列表数据处理
+      dataDispose(data){
+        data.forEach((val,index) => {
+          switch(val.status_code){
+            case 'at00':
+              val.status_code = '待支付'
+              break;
+            case 'wfd0':
+              val.status_code = '待分配'
+              break;
+            case 'wfs0':
+              val.status_code = '待发货'
+              break;
+            case 'wfd1':
+              val.status_code = '待收货'
+              break;
+            case 'wfc0':
+              val.status_code = '待评价'
+              break;
+            case 'trm0':
+              val.status_code = '已完成'
+              break;
+            case 'c000':
+              val.status_code = '已取消'
+              break;
+            case 'r000':
+              val.status_code = '退款中'
+              break;
+            case 'rs00':
+              val.status_code = '已退款'
+              break;
+          }
+          val.items.forEach((valChild)=>{
+            valChild.cover = `/attachment/${valChild.cover}`
+          })
+        })
+      }
     },
   }
   require('../assets/scss/myOrder.scss')
